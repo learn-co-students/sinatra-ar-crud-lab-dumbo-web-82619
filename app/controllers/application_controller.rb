78e7c1d508c292e7dd012bd95dbdc3dda06faa4f
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-09-18 17:33:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,6 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
     article = Article.find(params[:id])
     article.update(params[:article])
-    # article.update(title: params[:title], content: params[:content])
     redirect to "/articles/#{article.id}" 
   end
 
